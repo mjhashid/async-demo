0c90208a4009e5f0008d92b086209748a03ed126
Momohjimoh Hashidu <hashibibat@gmail.com>
2024-08-25 20:25:54 +0100

    Adding some profile details...

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ getUser(1, (user) => {
   getRepositories(user.gitHubUsername, (repos, status) => {
     console.log("Repos", repos);
     console.log("Status", status);
+    getCommits(user.gitHubUsername, (details) => {
+      console.log("Profile", details[0].email, details[1]);
+    });
   });
 });
 console.log("After");
@@ -23,9 +26,29 @@ function getUser(id, callback) {
 function getRepositories(username, callback) {
   setTimeout(() => {
     console.log("Calling GitHub API...");
+    // Applying two arguments to the callback function...
     callback(["repo1", "repo2", "repo3"], {
       name: "Momohjimoh Hashidu",
       level: "Junior Developer",
     });
+  }, 2000);
+}
+
+function getCommits(commit, callback) {
+  setTimeout(() => {
+    console.log("Loading Details...!");
+    callback([
+      (profile = {
+        name: "Momohimoh Hashidu",
+        email: "hashibibat@gmail.com",
+        institution: "Ahmadu Bello University Zaria",
+      }),
+      (address = {
+        country: "Nigeria",
+        state: "Kogi",
+        LGA: "Okene",
+        homeAddress: "No 24f Idoji Street Okene, Kogi State.",
+      }),
+    ]);
   }, 2000);
 }
