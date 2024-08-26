94d5f4d6efde2bce5f175b53724a9b82c4b6f25a
Momohjimoh Hashidu <hashibibat@gmail.com>
2024-08-26 15:22:31 +0100

    Asyn and Await approche..

--- callback-promises.js
+++ callback-promises.js
@@ -15,6 +15,19 @@ getUser(1)
   .then((commits) => console.log("Commits:", commits))
   .catch((err) => console.log("Error:", err.message));
 
+// Async and Await approach..
+async function displayCommits() {
+  try {
+    const user = await getUser(1);
+    const repos = await getRepositories(user.gitHubUsername);
+    const commits = await getCommits(repos[0]);
+    console.log(commits);
+  } catch (err) {
+    console.log("Error:", err.message);
+  }
+}
+displayCommits();
+
 console.log("After");
 
 function getUser(id) {
@@ -30,7 +43,8 @@ function getRepositories(username) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       console.log("Calling GitHub API...");
-      resolve(["repo1", "repo2", "repo3"]);
+      // resolve(["repo1", "repo2", "repo3"]);
+      reject(new Error("Could not get the Repos."));
     }, 2000);
   });
 }
